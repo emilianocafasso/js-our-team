3125b5e8aba5844b638e9a0cd9193cdd2e00e54b
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-20 15:53:32 +0200

    add column

--- assets/script.js
+++ assets/script.js
@@ -12,44 +12,53 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "assets/img/female3.png"
   }
 ];
 
 //seleziono il contenitore
-const container = document.getElementById('card-container')
+const container = document.getElementById('team-container')
 
 //ciclo per creare una card per ogni membro del team
 for(let i=0; i<teamMembers.length; i++) {
   const thisMember = teamMembers[i]
+
+  //creo la colonna bootstrap
+  const col = document.createElement('div')
+  col.className = 'col-md-4 mb-4' //3 colonne a partire da schermi medium
+  
+  //creo la card bootstrap
+  const card = document.createElement('div')
+  
+
 }
